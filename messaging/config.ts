be9d76704487fabe1328bffd54c4207b87a8b230
p.banaei <p.banaei@hqs.soshyant>
2025-12-02 17:45:49 +0330

    inital project

--- messaging/config.ts
+++ messaging/config.ts
@@ -1,14 +1,15 @@
 export const firebaseConfig = {
   // Your web app's Firebase configuration here
   // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID'
+
+
+  apiKey: "AIzaSyDIdkN0bpG1AIiwnZwMQ5bJKSzXL5tighY",
+  authDomain: "push-notif-78fd4.firebaseapp.com",
+  projectId: "push-notif-78fd4",
+  storageBucket: "push-notif-78fd4.firebasestorage.app",
+  messagingSenderId: "947574948078",
+  appId: "1:947574948078:web:bad3e481fa4b73987773ff",
+  measurementId: "G-0NKG5LW119"
 };
 
-export const vapidKey = '<YOUR_PUBLIC_VAPID_KEY_HERE>';
+export const vapidKey = 'BIYc7x4ZQ1qv5vOdXDVIxSp3VSgSVLoZIoaxNvOaXgPraPZEju3T-wL5rg89lkZp8-92Uw4j8M5HWK7yoz9Z-7c';
